4acc60c9c18c2e74492e08f6616c04c881784551
WalkHam <walkerocks@gmail.com>
2025-04-09 15:53:34 -0600

    Walker - Ambiences are working again
    Made the wwise implementation as well as the trigger game objects

--- Codename_IBS_Beta/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Codename_IBS_Beta/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,14 +14,17 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID AMB_HOSPITAL_LOOP = 3217822573U;
+        static const AkUniqueID ROOM_AMB = 3789107537U;
         static const AkUniqueID SFX_ENEMIES_BLOODCELL_DEATH = 1012708473U;
         static const AkUniqueID SFX_ENEMIES_BLOODCELL_HIT = 3902373752U;
         static const AkUniqueID SFX_ENEMIES_BLOODCELL_IDLE = 1098573287U;
         static const AkUniqueID SFX_ENEMIES_FATBLOB_DISSOLVE = 3801009704U;
         static const AkUniqueID SFX_ENEMIES_FATBLOB_IDLE = 3292684059U;
         static const AkUniqueID SFX_ENEMIES_GERMSLIME_DEATH = 102550024U;
+        static const AkUniqueID SFX_ENEMIES_GERMSLIME_HIT = 3369942825U;
         static const AkUniqueID SFX_ENEMIES_GERMSLIME_IDLE = 3055844788U;
         static const AkUniqueID SFX_ENEMIES_GERMSPIKE_DEATH = 1948922918U;
+        static const AkUniqueID SFX_ENEMIES_GERMSPIKE_HIT = 3347914759U;
         static const AkUniqueID SFX_ENEMIES_GERMSPIKE_IDLE = 2334927414U;
         static const AkUniqueID SFX_WEAPON_ALL_SWAP = 3791724757U;
         static const AkUniqueID SFX_WEAPON_GERMOBLASTER_FIRE = 1631753596U;
@@ -33,6 +36,23 @@ namespace AK
         static const AkUniqueID SFX_WEAPON_PILL_THROW = 1104292844U;
         static const AkUniqueID TEST_POP = 3024126497U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ROOM_AMB
+        {
+            static const AkUniqueID GROUP = 3789107537U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LARGE = 4284352190U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SMALL = 1846755610U;
+                static const AkUniqueID STOMACH = 1434060888U;
+            } // namespace STATE
+        } // namespace ROOM_AMB
+
+    } // namespace STATES
 
     namespace BANKS
     {
